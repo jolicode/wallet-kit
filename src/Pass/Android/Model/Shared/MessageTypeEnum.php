9a898a19042d1963c4389577cf8f962f710c3a0b
Baptiste Leduc <baptiste.leduc@gmail.com>
2026-04-09 22:22:35 +0200

    Update Google Wallet spec to revision 20260409
    Add missing enum values introduced in the latest Google Wallet discovery API:
    - GenericTypeEnum: BARCODE_PASS, BUSINESS_CARD, MEMBERSHIP_CARD, STUDENT_CARD, TRANSIT_PASS, VEHICLE_REGISTRATION
    - MessageTypeEnum: MESSAGE_TYPE_UNSPECIFIED, EXPIRATION_NOTIFICATION
    - StateEnum: STATE_UNSPECIFIED
    - BarcodeTypeEnum: BARCODE_TYPE_UNSPECIFIED, EAN13, PDF417 (legacy aliases)

--- src/Pass/Android/Model/Shared/MessageTypeEnum.php
+++ src/Pass/Android/Model/Shared/MessageTypeEnum.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Jolicode\WalletKit\Pass\Android\Model\Shared;
 
 /**
- * @phpstan-type MessageType 'TEXT'|'TEXT_AND_NOTIFY'
+ * @phpstan-type MessageType 'MESSAGE_TYPE_UNSPECIFIED'|'TEXT'|'TEXT_AND_NOTIFY'|'EXPIRATION_NOTIFICATION'
  */
 enum MessageTypeEnum: string
 {
+    case UNSPECIFIED = 'MESSAGE_TYPE_UNSPECIFIED';
     case TEXT = 'TEXT';
     case TEXT_AND_NOTIFY = 'TEXT_AND_NOTIFY';
+    case EXPIRATION_NOTIFICATION = 'EXPIRATION_NOTIFICATION';
 }
