5d6076306df4e5992aabf781dc5feadd75230eae
Baptiste Leduc <baptiste.leduc@gmail.com>
2026-04-20 23:45:28 +0200

    Add Google Wallet API client and save link generator

---
+++ src/Api/Google/GoogleSaveLinkGenerator.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jolicode\WalletKit\Api\Google;
+
+use Jolicode\WalletKit\Api\Credentials\GoogleCredentials;
+use Jolicode\WalletKit\Builder\GoogleVerticalEnum;
+use Jolicode\WalletKit\Builder\GoogleWalletPair;
+use Jolicode\WalletKit\Exception\Api\AuthenticationException;
+use Jolicode\WalletKit\Exception\Api\MissingExtensionException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class GoogleSaveLinkGenerator
+{
+    public function __construct(
+        private readonly NormalizerInterface $normalizer,
+        private readonly GoogleCredentials $credentials,
+    ) {
+        if (!\extension_loaded('openssl')) {
+            throw new MissingExtensionException('The "openssl" PHP extension is required for Google Save Link generation.');
+        }
+    }
+
+    /**
+     * @return string https://pay.google.com/gp/v/save/{jwt}
+     */
+    public function generateSaveLink(GoogleWalletPair $pair): string
+    {
+        $serviceAccount = $this->credentials->getServiceAccountData();
+
+        $clientEmail = $serviceAccount['client_email'] ?? null;
+        $privateKey = $serviceAccount['private_key'] ?? null;
+
+        if (!\is_string($clientEmail) || !\is_string($privateKey)) {
+            throw new AuthenticationException('Service account JSON must contain "client_email" and "private_key" fields.');
+        }
+
+        $normalizedObject = $this->normalizer->normalize($pair->passObject);
+        $payloadKey = self::objectsPayloadKey($pair->vertical);
+
+        $header = self::base64UrlEncode(json_encode(['alg' => 'RS256', 'typ' => 'JWT'], \JSON_THROW_ON_ERROR));
+        $claims = self::base64UrlEncode(json_encode([
+            'iss' => $clientEmail,
+            'aud' => 'google',
+            'typ' => 'savetowallet',
+            'payload' => [
+                $payloadKey => [$normalizedObject],
+            ],
+        ], \JSON_THROW_ON_ERROR));
+
+        $signingInput = $header . '.' . $claims;
+
+        $key = openssl_pkey_get_private($privateKey);
+
+        if (false === $key) {
+            throw new AuthenticationException('Unable to read private key from Google service account.');
+        }
+
+        $signature = '';
+
+        if (!openssl_sign($signingInput, $signature, $key, \OPENSSL_ALGO_SHA256)) {
+            throw new AuthenticationException(\sprintf('Failed to sign save link JWT: %s', openssl_error_string() ?: 'unknown error'));
+        }
+
+        $jwt = $signingInput . '.' . self::base64UrlEncode($signature);
+
+        return 'https://pay.google.com/gp/v/save/' . $jwt;
+    }
+
+    private static function objectsPayloadKey(GoogleVerticalEnum $vertical): string
+    {
+        return match ($vertical) {
+            GoogleVerticalEnum::FLIGHT => 'flightObjects',
+            GoogleVerticalEnum::EVENT_TICKET => 'eventTicketObjects',
+            GoogleVerticalEnum::GENERIC => 'genericObjects',
+            GoogleVerticalEnum::GIFT_CARD => 'giftCardObjects',
+            GoogleVerticalEnum::LOYALTY => 'loyaltyObjects',
+            GoogleVerticalEnum::OFFER => 'offerObjects',
+            GoogleVerticalEnum::TRANSIT => 'transitObjects',
+        };
+    }
+
+    private static function base64UrlEncode(string $data): string
+    {
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+    }
+}
